de5fd2fe107b1426d5bb5100b44569c34a3f8bbf
iolinpic <iolinpic@gmail.com>
2019-08-08 18:10:38 +0300

    add config file

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // proxy API requests to Valet during development
+    devServer: {
+        // proxy: 'http://192.168.10.10'
+        // proxy:process.env.VUE_APP_PROXY,
+        // public:process.env.VUE_APP_PROXY,
+        // proxy: 'http://yusdoc.ru'
+    },
+
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    // outputDir: '../public',
+
+    // modify the location of the generated HTML file.
+    // make sure to do this only in production.
+    // indexPath: process.env.NODE_ENV === 'production'
+    //     ? '../resources/views/index.blade.php'
+    //     : 'index.html'
+};
