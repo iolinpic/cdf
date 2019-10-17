008a51e88c5e8af3d5fde8a379ff4827b83f929b
iolinpic <iolinpic@gmail.com>
2019-10-17 15:56:11 +0300

    ability pack api

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,12 @@ import auth from './auth'
 import user from './user'
 import conditions from  './conditions'
 import abilities from  './abilities'
+import aPack from  './abilityPack'
 
 export default {
     conditions,
     auth,
     user,
     abilities,
+    aPack,
 }
--- src/components/AbilityConditionComponent.vue
+++ src/components/AbilityConditionComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout wrap>
         <v-flex sm6>
-            <v-select :items="conditions" v-model="obj.Name" item-value="id" item-text="DisplayNameText" label="Состояние"></v-select>
+            <v-autocomplete :items="conditions" v-model="obj.Name" item-value="id" item-text="DisplayNameText" label="Состояние"></v-autocomplete>
         </v-flex>
         <v-flex sm5>
             <v-text-field label="Шанс применения" :value="obj.Chance" @input="updData"></v-text-field>
