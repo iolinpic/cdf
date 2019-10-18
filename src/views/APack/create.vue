e1d8b14ff1780f710573416ac40272cddcc37442
iolinpic <iolinpic@gmail.com>
2019-10-18 12:46:56 +0300

    condition settings change:  - exclude resistances from setup  - add purification (type 19)  configs

--- src/views/APack/create.vue
+++ src/views/APack/create.vue
@@ -26,7 +26,7 @@
     export default {
         name: 'AbilityPackCreate',
         metaInfo: {
-            title: 'Создание новой способности',
+            title: 'Создание нового пула способностей',
         },
         components: {
             VTextField,
