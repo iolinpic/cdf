54bd9d5398341e9056cd46611830b3375de4fb85
iolinpic <iolinpic@gmail.com>
2020-11-12 13:15:24 +0300

    dialog greetings preparation config

---
+++ src/components/dialogs/actorDialogConditionComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-layout wrap>
+        <template v-if="keys.length>=1">
+            <v-flex sm12 v-for="key in keys" :key="key">
+                <div :is="settings[key].component" :items="settings[key].items?settings[key].items:''"
+                     :label="settings[key].label" v-model="value[key]"></div>
+            </v-flex>
+        </template>
+    </v-layout>
+</template>
+<script>
+    import {VTextField, VSwitch, VSelect} from 'vuetify/lib'
+    import OptionComponent from "../OptionComponent";
+    import itemSelectComponent from "../itemSelectComponent";
+    import CrysmTypeComponent from "../CrysmTypeComponent";
+    import CrysmSelectComponent from "../crysmSelect"
+    import ZoneSelectComponent from "../zone/selectSingle";
+    import QuestSelectComponent from "./QuestSelectComponent";
+
+    export default {
+        name: 'actorDialogConditionComponent',
+        data() {
+            return {
+            };
+        },
+        props: {
+            value: Object,
+            items: Array,
+            quests: Array,
+            zones: Array,
+            //keys:Array,
+            settings:Object,
+        },
+        components:{
+            OptionComponent,
+            VTextField,
+            VSwitch,
+            VSelect,
+            itemSelectComponent,
+            CrysmTypeComponent,
+            CrysmSelectComponent,
+            ZoneSelectComponent,
+            QuestSelectComponent,
+        },
+        computed: {
+            keys() {
+                return Object.keys(this.value);
+            },
+            /*settings() {
+                return DialogGreetingsSettings;
+            },*/
+        },
+        mounted() {
+
+        },
+        methods: {
+        }
+    }
+</script>
---
+++ src/components/dialogs/actorDialogsComponent.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <v-layout row class="pt-10">
+            <v-flex sm10> <div class="title">Диалоги</div></v-flex>
+            <v-flex sm2> <v-btn color="blue" @click="addValue">Добавить</v-btn></v-flex>
+        </v-layout>
+
+        <v-divider class="mb-3"></v-divider>
+        <v-progress-linear indeterminate v-if="loading"></v-progress-linear>
+        <template v-else>
+            <v-expansion-panels>
+                <v-expansion-panel
+                        v-for="(item,index) in value"
+                        :key="'dialog_'+index"
+                >
+                    <v-expansion-panel-header>
+                        <span>{{item.Name}} - приоритет: {{item.Priority}}</span>
+                        <div>
+
+                            <v-btn icon @click="removeValue(index)">
+                                <v-icon color="red">delete</v-icon>
+                            </v-btn>
+                        </div>
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <actor-dialog-component
+                                :value="item"
+
+                        ></actor-dialog-component>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </template>
+    </div>
+</template>
+<script>
+    import { DialogGreetingsValues} from '@/config/actors'
+    import actorDialogComponent from "./actorDialogComponent";
+    //import api from "@/api";
+
+    export default {
+        name: 'actorDialogsComponent',
+        components: {actorDialogComponent},
+        data() {
+            return {
+                loading: false,
+                items: [],
+                zones: [],
+                quests: [],
+                crysms: [],
+            };
+        },
+        props: {
+            value: Array,
+        },
+        computed: {
+        },
+        mounted() {
+            //this.getResources();
+        },
+
+        methods: {
+           /* getResources() {
+                this.loading = true;
+                let itemLoading = true;
+                let zoneLoading = true;
+                let questLoading = true;
+                let crysmLoading = true;
+                api.items.all().then((res) => {
+                    this.items = res.data;
+                    itemLoading = false;
+                    if(!(itemLoading&&zoneLoading&&questLoading&&crysmLoading)){
+                        this.loading=false;
+                    }
+                });
+                api.zones.all().then((res)=>{
+                    this.zones=res.data;
+                    zoneLoading = false;
+                    if(!(itemLoading&&zoneLoading&&questLoading&&crysmLoading)){
+                        this.loading=false;
+                    }
+                })
+                api.quests.all().then((res)=>{
+                    this.quests = res.data;
+                    questLoading = false;
+                    if(!(itemLoading&&zoneLoading&&questLoading&&crysmLoading)){
+                        this.loading=false;
+                    }
+                })
+                api.crysms.all().then((res)=>{
+                    this.crysms = res.data;
+                    crysmLoading = false;
+                    if(!(itemLoading&&zoneLoading&&questLoading&&crysmLoading)){
+                        this.loading=false;
+                    }
+                })
+            },*/
+            addValue() {
+                this.value.push(Object.assign({},DialogGreetingsValues));
+            },
+            removeValue(index) {
+                this.value.splice(index, 1);
+            },
+        }
+    }
+</script>
