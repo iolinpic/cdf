936e46b3c68f3d0374e6c5488f8cfcf77d09602f
iolinpic <iolinpic@gmail.com>
2020-12-22 11:06:36 +0300

    draw component

---
+++ src/components/draw/drawComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        name: 'DrawComponent',
+        data() {
+            return {
+            }
+        },
+        props: {
+        },
+        components: {
+        },
+        mounted(){
+        },
+        methods: {
+
+        }
+    }
+</script>
