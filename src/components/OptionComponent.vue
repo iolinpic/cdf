b2e30573ec0e2f645137677d49f6c2300085e23b
iolinpic <iolinpic@gmail.com>
2021-01-26 19:24:39 +0300

    fixed NaN filler

--- src/components/OptionComponent.vue
+++ src/components/OptionComponent.vue
@@ -1,18 +1,22 @@
 <template>
-    <v-text-field :value="value" @input="updData" :label="label"></v-text-field>
+  <v-text-field :value="value" @input="updData" :label="label"></v-text-field>
 </template>
 <script>
-    export default {
-        name: 'OptionComponent',
-        props: {
-            value: Number,
-            label: String,
-        },
-        methods: {
-            updData(val) {
-                const tmp = parseFloat(val);
-                this.$emit('input',tmp);
-            },
-        },
-    }
+export default {
+  name: 'OptionComponent',
+  props: {
+    value: Number,
+    label: String,
+  },
+  methods: {
+    updData(val) {
+
+      let tmp = parseFloat(val);
+      if (Number.isNaN(tmp)) {
+        tmp = ''
+      }
+      this.$emit('input', tmp);
+    },
+  },
+}
 </script>
